272d5904e50d9b8a231066116c8296bda0430b35
Jessica Xia <jxia@node21.camhres.ca>
2022-07-25 13:32:37 -0400

    Reads in already filtered published list of GWAS instead of raw

--- GWAS_sumstats_prep.R
+++ GWAS_sumstats_prep.R
@@ -1,52 +1,64 @@
 library(data.table)
 library(dplyr)
 library(h2o)
+library(readxl)
+library(stringr)
 
-#Sumstat preparation
+#GWAS summary statistics preparation, standardizes column names for each GWAS, ensures RSID is matched to chromosomal location
 #NOTE: Different GWAS papers provides the raw sumstat file in different formats
-setwd('/external/rprshnas01/netdata_kcni/stlab/Alex_PRS/sumstats/raw_sumstats') #Original files are stored here
-standard_colnames=c("SNP","CHR","A1","A2","P") #Standardized column names to be used for each GWAS
+setwd('/external/rprshnas01/kcni/jxia/chromatin-accessibiliy') #Original files are stored here
+standard_colnames=c("SNP","CHR", "BP", "A1","A2","P") #Standardized column names to be used for each GWAS
 
 
 #Schizophrenia (original paper:'https://doi.org/10.1038/s41586-022-04434-5')
-SZ <- fread("PGC3_SCZ_wave3.european.autosome.public.v3.vcf.tsv.gz") %>%
-  mutate(., freq=(FCAS*NCAS+FCON*NCON)/(NCAS+NCON)) %>% #Calculate average frequency of the risk allele across cases and controls
-  mutate(., MAF=ifelse(freq > .5, 1-freq, freq)) %>% #If freq of allele of interest indicates its the major allele, subtract from 1 to calculate minor allele frequency (MAF), otherwise leave as is. 
-  filter(., MAF>=0.01 & PVAL<5*10^-8 & IMPINFO>=0.8) %>% #Filter out SNPs with MAF<1% (standard)
-  select(., c('ID','CHROM','A1','A2','PVAL')) #Columns kept in final dataframe
+SZ <- read_xlsx("/external/rprshnas01/kcni/jxia/chromatin-accessibiliy/SZ_gwas_sum.xlsx", sheet=2) %>%
+  filter(., `P-comb`<5*10^-8) %>% #P-comb = p-value of combined discover-replication meta analysis. Filter out SNPs with P-value less than 5*10^-8, yields 342 SNPs
+  mutate(A1=str_match(A1A2, "([A-Z])/([A-Z])")[,2],
+         A2=str_match(A1A2, "([A-Z])/([A-Z])")[,3]) %>%
+  select(., c('SNP','CHR', 'BP', 'A1', 'A2','P-comb')) #Columns kept in final dataframe
 
-colnames(SZ)=standard_colnames #To standardize the names of columns
-fwrite(SZ, file = "YOURFOLDER/SZ.sumstats",sep="\t",quote=FALSE,row.names=FALSE,col.names=TRUE)
+colnames(SZ) <- standard_colnames #To standardize the names of columns
+saveRDS(SZ, file = "SZ_gwas_final_list.rds") #Save as a single R object
 
 
 #Bipolar disorder (original paper:'https://doi.org/10.1038/s41588-021-00857-4')
-BD <- fread("pgc.bip.full.2012-04.txt.gz") #Error when loading, addressed in next line of code
-colnames(BD) <- c("SNP","CHR","BP","A1","A2","OR","SE","P","INFO","NGT","CEUaf") #Fixes the error
+BD <- read_xlsx("/external/rprshnas01/kcni/jxia/chromatin-accessibiliy/BD_gwas_sum.xlsx", sheet=3) %>% #p-values are already filtered for P<5×10^−8
+  row_to_names(row_number = 1) %>%
+  mutate(A1=str_match(`A1/A2`, "([A-Z])/([A-Z])")[,2],
+         A2=str_match(`A1/A2`, "([A-Z])/([A-Z])")[,3]) %>%
+  select(., c('SNP','CHR', 'BP', 'A1', 'A2','P')) %>%
+  head(.,-5) # Last 5 rows are descriptions
 
-ref <- fread("reference.1000G.maf.0.005.txt.gz",
-             select = c("SNP", "MAF")) #CEUaf=frequency of A1 but is missing many values. Use this reference instead
-
-BD <- BD %>% left_join(.,ref,by="SNP") %>% #Combine ref to BD sumstats raw file
-  filter(., MAF>=0.01 & P<5*10^-8 & INFO>=0.8) %>%
-  select(.,c("SNP","CHR","A1","A2","P"))
-
-fwrite(BD, file = "~/BD.sumstats",sep="\t",quote=FALSE,row.names=FALSE,col.names=TRUE)
+BD[ , c('BP', 'P')] <- apply(BD[ , c('BP', 'P')],2, #2 specifies apply is applied by column
+                                    function(x){as.numeric(as.character(x))}) #Convert specified columns to numeric type
+colnames(BD) <- standard_colnames
+saveRDS(BD, file = "BD_gwas_final_list.rds")
 
 
+#Depression (original paper:'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6522363/')
+MDD <- read_xlsx("/external/rprshnas01/kcni/jxia/chromatin-accessibiliy/MDD_gwas_sum.xlsx") %>%
+  subset(select=c(2,1,3,4,5,31)) %>%
+  row_to_names(row_number = 2)
 
-#Depression (original paper:'https://doi.org/10.7488/ds/2458')
-MDD<-fread("PGC_UKB_depression_genome-wide.txt") %>%
-  mutate(., MAF=ifelse(Freq > .5, 1-Freq, Freq)) %>% #Convert Freq to MAF, same as before
-  filter(., MAF>=0.01 & P<5*10^-8) %>% #There's no INFO column this time, only filtering by MAF & P value
-  select(., c("MarkerName","A1","A2","P"))
-
+colnames(MDD) <- standard_colnames
 MDD$A1 <- toupper(MDD$A1) #Convert A1 and A2 to uppercase like the other dataframes
 MDD$A2 <- toupper(MDD$A2)
+MDD$BP <- as.numeric(as.character(MDD$BP))
+MDD$P <- as.numeric(as.character(MDD$P))
 
-colnames(MDD)=standard_colnames
-fwrite(MDD, file = "YOURFOLDER/MDD.sumstats",sep="\t",quote=FALSE,row.names=FALSE,col.names=TRUE)
+saveRDS(MDD, file = "MDD_gwas_final_list.rds")
 
 
+'''
+#Intersects gwas summary with rsid file, assigns chromosomal location for each rsid entry in gwas summary statistics. fread is for regular delimited files; i.e., where every row has the same number of columns. delimited flat file contains one or more records set off from each other by a specified delimiter, or separator.
+scz_withloc <- fread("/external/rprshnas01/netdata_kcni/stlab/Alex_PRS/annotation/HRC.r1-1.GRCh37.wgs.mac5.sites.vcf.gz",
+                     select = c("#CHROM", "POS", "ID", "REF", "ALT")) %>%
+  rename("SNP"="ID") %>% #Rename rsid column to be the same as in scz
+#Reference file that matches RSids to chromosomal locations for GWAS summary statistics
+rsid_ref <- fread("/external/rprshnas01/netdata_kcni/stlab/Alex_PRS/annotation/HRC.r1-1.GRCh37.wgs.mac5.sites.vcf.gz",
+                  select = c("#CHROM", "POS", "ID", "REF", "ALT"))
+rsid_ref <- rename(rsid_ref, "SNP"="ID") #Rename rsid column to be the same as in the summary statistics. Get error related to not enough memory when combined with above
+'''
 
 ######with these you should be able to prepare IQ, EA, CP and OCD
 IQ<-fread("SavageJansen_2018_intelligence_metaanalysis.txt")
