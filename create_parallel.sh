029b1b25e256af3bd7b97c99d0e513163b446e63
Jessica Xia <jxia@scclogin01.camhres.ca>
2022-07-26 23:18:23 -0400

    Shell script to create parallel scripts for each SNP

---
+++ create_parallel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+trait=$1
+folder_name=$2
+
+for i in {1..342}
+do 
+  echo "#!/bin/bash" > "/nethome/kcni/jxia/chromatin-accessibility/${folder_name}/${trait}_${i}.sh"
+  echo "Rscript /nethome/kcni/jxia/chromatin-accessibility/parallel.R ${i}" >> "/nethome/kcni/jxia/chromatin-accessibility/${folder_name}/${trait}_${i}.sh"
+  echo "/nethome/kcni/jxia/chromatin-accessibility/${folder_name}/${trait}_${i}.sh" >> "/nethome/kcni/jxia/chromatin-accessibility/${folder_name}/script_list.txt" 
+  chmod +x /nethome/kcni/jxia/chromatin-accessibility/${folder_name}/${trait}_${i}.sh
+  echo "File ${trait}_${i}.sh generated"  
+done
